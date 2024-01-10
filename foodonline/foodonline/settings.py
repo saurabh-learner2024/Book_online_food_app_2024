5e18defcd37fdf4d9d203b8a072f4f19480162d3
saurabh-learner2024 <saurabh.jaiswal0096@gmail.com>
2024-01-10 20:32:54 +0530

    restaurant profile + google geocoding

--- foodonline/foodonline/settings.py
+++ foodonline/foodonline/settings.py
@@ -64,6 +64,7 @@ TEMPLATES = [
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
                 'accounts.context_processors.get_vendor',
+                'accounts.context_processors.get_google_api',
             ],
         },
     },
@@ -144,3 +145,5 @@ EMAIL_HOST_USER = config('EMAIL_HOST_USER')
 EMAIL_HOST_PASSWORD = config('EMAIL_HOST_PASSWORD')
 EMAIL_USE_TLS = config('EMAIL_USE_TLS')
 DEFAULT_FROM_EMAIL = 'foodOnline Marketplace <s452465889@gmail.com>'
+
+GOOGLE_API_KEY = 'AIzaSyAFJpG9HeceAB9VzRlmRAGi7DuwkW6mILg'
